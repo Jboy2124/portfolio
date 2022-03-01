137e2f4d6359b260e2bfc5fd447510730bfcaa5a
Jufel Abecia <jufelabecia@gmail.com>
2022-03-01 22:55:37 +0800

    message

--- js/main.js
+++ js/main.js
@@ -1,52 +1,52 @@
-let tasks = [
-    {
-        title: "Programming Languages",
-        text: "The quick brown fox jump over the",
-    },
-    {
-            title: "Front-End",
-            text: "The The Lazy dog",
-    },
-    {
-        title: "Back-End",
-        text: "The The Lazy dog asdjashdjashddsafsafadf",
-    },
-];
+// let tasks = [
+//     {
+//         title: "Programming Languages",
+//         text: "The quick brown fox jump over the",
+//     },
+//     {
+//             title: "Front-End",
+//             text: "The The Lazy dog",
+//     },
+//     {
+//         title: "Back-End",
+//         text: "The The Lazy dog asdjashdjashddsafsafadf",
+//     },
+// ];
 
-let cardContainer;
-let createTaskCard = (task) => {
+// let cardContainer;
+// let createTaskCard = (task) => {
 
-let card = document.createElement("div");
-card.className = "card";
+// let card = document.createElement("div");
+// card.className = "card";
 
-let cardBody = document.createElement("div");
-cardBody.className = "card-body";
+// let cardBody = document.createElement("div");
+// cardBody.className = "card-body";
 
-let title = document.createElement("h5");
-title.innerText = task.title;
-title.className = "card-title";
+// let title = document.createElement("h5");
+// title.innerText = task.title;
+// title.className = "card-title";
 
-let cText = document.createElement('p');
-cText.innerText = task.text;
-cText.className = 'card-text';
+// let cText = document.createElement('p');
+// cText.innerText = task.text;
+// cText.className = 'card-text';
 
-cardBody.appendChild(title);
-cardBody.appendChild(cText);
-card.appendChild(cardBody);
-cardContainer.appendChild(card);
+// cardBody.appendChild(title);
+// cardBody.appendChild(cText);
+// card.appendChild(cardBody);
+// cardContainer.appendChild(card);
 
-}
+// }
 
-let initListOfTasks = () => {
-if (cardContainer) {
-    document.getElementById('card-container').replaceWith(cardContainer);
-    return;
-}
+// let initListOfTasks = () => {
+// if (cardContainer) {
+//     document.getElementById('card-container').replaceWith(cardContainer);
+//     return;
+// }
 
-cardContainer = document.getElementById('card-container');
-tasks.forEach((task) => {
-    createTaskCard(task);
-    });
-};
+// cardContainer = document.getElementById('card-container');
+// tasks.forEach((task) => {
+//     createTaskCard(task);
+//     });
+// };
 
-initListOfTasks();
+// initListOfTasks();
